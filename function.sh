44af99e6dc97ea45fae8bfa1041cb6e574a23784
Soft Breeze <softbreezeen@aol.com>
2012-02-27 12:44:07 +0100

    Added function to send without attachement.

--- function.sh
+++ function.sh
@@ -2,3 +2,8 @@ function fsendemail {
 RESP=`sendemail -f from.email@yahoo.com -t to.email@address.com -s smtp.mail.yahoo.com:587 -u "$COMP_ID - $1" -m "$2" -a "$3"  -xu "user" -xp pass`
 }
 
+function fsendemail_na {
+RESP=`sendemail -f from.email@yahoo.com -t to.email@address.com -s smtp.mail.yahoo.com:587 -u "$COMP_ID - $1" -m "$2" -a "$3"  -xu "user" -xp pass`
+}
+
+
