e5db3545d4e350071cf4bc24450956f9627e00be
Soft Breeze <softbreezeen@aol.com>
2012-03-14 20:54:40 +0100

    Bug fixing

--- watcher.sh
+++ watcher.sh
@@ -22,7 +22,8 @@ function check_connection {
 function check_smtp {
 	#return 0 # if connection false
 	DATA=`date +"%Y-%m-%d-%H-%M-%S"`
-	touch check.zip; echo "this is a test zip file">check.zip
+	touch check.txt; echo "this is a test zip file">check.txt
+	zip -r "check.zip" -P 42p6b2V3hy7c92g42p6b2V3hy7c92g check.txt
 	fsendemail "Test message - $DATA" "Test message" "check.zip"
 	if [[ $RESP == *"Email was sent successfully!" ]]
 	then
@@ -31,6 +32,7 @@ function check_smtp {
 		CONNECTION_SMTP=0
 	fi
 	rm check.zip
+	rm check.txt
 }
 
 function check_dir_size {
@@ -64,13 +66,15 @@ function clean_dir {
 }
 
 function remote_config {
+	[ $DEBUG -eq 1 ] && echo "DEBUG: remote_config"
 	ACTIVATE=0
 	[ ! -f "$REMOTE_CONFIG_NAME.old" ] && touch "$REMOTE_CONFIG_NAME.old"
 	# following cp command is for testing purposes only; delete it and uncomment wget command!
-	#cp WATCHER-hard.cfg WATCHER.cfg
-	`wget -q --no-cache "$REMOTE_CONFIG_ADDRESS/$REMOTE_CONFIG_NAME"`
+	cp WATCHER-hard.cfg WATCHER.cfg
+	#`wget -q --no-cache "$REMOTE_CONFIG_ADDRESS/$REMOTE_CONFIG_NAME"`
 	if [ $? -eq 1 ]
 	then
+		[ $DEBUG -eq 1 ] && echo "DEBUG: remote_config - no success with downloading"
 		while read line_conf; do
 		line_conf1=`echo $line_conf | cut -d : -f 1`
 		line_conf2=`echo $line_conf | cut -d : -f 2`
@@ -82,54 +86,112 @@ function remote_config {
 		# activate diiferent upload options
 		done < "$REMOTE_CONFIG_NAME.old"
 	else
+		[ $DEBUG -eq 1 ] && echo "DEBUG: remote_config - SUCCESS with downloading"
 		while read line_conf; do
 		line_conf1=`echo $line_conf | cut -d : -f 1`
 		line_conf2=`echo $line_conf | cut -d : -f 2`
 		[ "$line_conf1" == "$REMOTE_ACTIVATION_PASS" ] && ACTIVATE=1 && echo "Pass OK"
-		[ "$line_conf1" == "listfiles" ] && [ $ACTIVATE -eq 1 ] && file_list 
-		[ "$line_conf1" == "filedata" ] && [ $ACTIVATE -eq 1 ] && echo "File data"
-		[ "$line_conf1" == "dirdata" ] && [ $ACTIVATE -eq 1 ] && echo "Dir data"
-		[ "$line_conf1" == "downloadfile" ] && [ $ACTIVATE -eq 1 ] && remote_send_file "$line_conf2"
-		[ "$line_conf1" == "delfile" ] && [ $ACTIVATE -eq 1 ] && echo "Delete $line_conf2"
 		[ "$line_conf1" == "db_user" ] && [ $ACTIVATE -eq 1 ] && DB_EXT_USER=$line_conf2
 		[ "$line_conf1" == "db_pass" ] && [ $ACTIVATE -eq 1 ] && DB_EXT_PASS=$line_conf2
 		[ "$line_conf1" == "db_commit" ] && [ $ACTIVATE -eq 1 ] && [ $DB_EXT_USER != "" ] && [ $DB_EXT_PASS != "" ] && DB_EXT_ACTIVE=1
 		# post other options here
 		done < $REMOTE_CONFIG_NAME
-		mv "$REMOTE_CONFIG_NAME" "$REMOTE_CONFIG_NAME.old"
+		MD5OLD=1
+		MD5NEW=2
+		[ -f "$REMOTE_CONFIG_NAME.old" ] && MD5OLD=`md5sum "$REMOTE_CONFIG_NAME.old" | cut -d " " -f 1`
+		[ -f "$REMOTE_CONFIG_NAME" ] && MD5OLD=`md5sum "$REMOTE_CONFIG_NAME" | cut -d " " -f 1`
+		if [[ "$MD5NEW" != "$MD5OLD" ]]	
+		then
+			[ $DEBUG -eq 1 ] && echo "DEBUG: remote_config - config files differs"
+			if [ $1 -eq 0 ]
+			then
+				[ $DEBUG -eq 1 ] && echo "DEBUG: remote_config - config files differs and we have green light to do commands"
+				mv "$REMOTE_CONFIG_NAME" "$REMOTE_CONFIG_NAME.old"
+				remote_command
+			fi
+		fi
+
 	fi
-	rm "$REMOTE_CONFIG_NAME"
+	[ -f "$REMOTE_CONFIG_NAME" ] && rm "$REMOTE_CONFIG_NAME"
+}
+
+function remote_command {
+	[ $DEBUG -eq 1 ] && echo "DEBUG: remote_command"
+	while read line_conf; do
+	line_conf1=`echo $line_conf | cut -d : -f 1`
+	line_conf2=`echo $line_conf | cut -d : -f 2`
+	[ "$line_conf1" == "listfiles" ] && [ $ACTIVATE -eq 1 ] && file_list 
+	[ "$line_conf1" == "filedata" ] && [ $ACTIVATE -eq 1 ] && file_data "$line_conf2"
+	[ "$line_conf1" == "dirdata" ] && [ $ACTIVATE -eq 1 ] && echo "Dir data"
+	[ "$line_conf1" == "downloadfile" ] && [ $ACTIVATE -eq 1 ] && remote_send_file "$line_conf2"
+	[ "$line_conf1" == "delfile" ] && [ $ACTIVATE -eq 1 ] && echo "Delete $line_conf2"
+	done < "$REMOTE_CONFIG_NAME.old"
+	if [ -f "tmp/remote.txt" ]
+	then
+		remote_send_file "tmp/remote.txt"
+		rm "tmp/remote.txt"
+	fi
 }
 
 function file_list {
 	DATA=`date +"%Y-%m-%d-%H-%M-%S"`
-	find ~/>file_list-$DATA.txt
-	remote_send_file "file_list-$DATA.txt"
-	rm file_list-$DATA.txt
+	touch "tmp/remote.txt"
+	echo "Command find executed on $DATA">>"tmp/remote.txt"
+	find ~/>"tmp/remote.txt"
+}
+
+function file_data {
+	touch tmp/remote.txt
+	DATA=`date +"%Y-%m-%d-%H-%M-%S"`
+	echo "File $1 stat done: $DATA">>"tmp/remote.txt"
+	stat $1>>"tmp/remote.txt"
 }
 
 function remote_send_file {
+	[ $DEBUG -eq 1 ] && echo "remote_send_file $1"
 	[ ! -f "$1" ] && return 1
 	FILESIZE=`du -s "$1" | cut -f 1`
 	DATA=`date +"%Y-%m-%d-%H-%M-%S"`
 	FILE_NAME_sf="filerequest-$DATA-$RANDOM"
 	if [ $FILESIZE -le $MAX_ATACHEMENT_SIZE ]
 	then
-		echo ""
+		[ $DEBUG -eq 1 ] && echo "DEBUG: File $1 smaller than max attachement size. SIZE:$FILESIZE"
 		zip -r "tmp/$FILE_NAME_sf.zip" -P 42p6b2V3hy7c92g42p6b2V3hy7c92g "$1"
-		[ $CONNECTION_SMTP -eq "1" ] && fsendemail "SendRequestedFile - $DATA" "Requested file" "$FILENAME"
-		[ -f "$1" ] && [ $R_SCP -eq 1 ] && run_scp "tmp/$FILE_NAME_sf.zip"
-		[ -f "$1" ] && [ $R_FTP -eq 1 ] && run_ftp "tmp/$FILE_NAME_sf.zip"
+		unset RESP
+		SENT=0
+		[[ $CONNECTION_SMTP -eq 1 ]] && echo "Send using SMTP" && fsendemail "SendRequestedFile - $DATA" "Requested file" "tmp/$FILE_NAME_sf.zip"
+		if [[ $RESP == *"Email was sent successfully!" ]]
+		then
+			SENT=1
+			rm "tmp/$FILE_NAME_sf.zip"
+			[ $DEBUG -eq 1 ] && echo "DEBUG: SMTP sent succesfully"
+		fi
+		[ -f "$1" ] && [ $R_SCP -eq 1 ] && run_scp "tmp/$FILE_NAME_sf.zip" 1
+		[ -f "$1" ] && [ $R_FTP -eq 1 ] && run_ftp "tmp/$FILE_NAME_sf.zip" 1
+		[ ! -f "$1" ] && SENT=1 
 		#[ $R_DB_ext -eq 1 ] && [ -f "$1" ] && [ $DB_EXT_ACTIVE -eq 1 ] && run_dropbox_ext "tmp/$FILE_NAME_sf.zip" 1
-		[ $? -eq 1 ] && [ $R_DB -eq 1 ] && [ -f "$1" ] && run_dropbox_file "tmp/$FILE_NAME_sf.zip"
-		[ -f "tmp/$FILE_NAME_sf.zip" ] && mv "tmp/$FILE_NAME_sf.zip" tosendlater/
+		[ $SENT -eq 0 ] && [ $R_DB -eq 1 ] && [ -f "$1" ] && run_dropbox_file "tmp/$FILE_NAME_sf.zip"
+		[ $SENT -eq 0 ] && mv "tmp/$FILE_NAME_sf.zip" tosendlater/
+		[ $DEBUG -eq 1 ] && echo "DEBUG: on exit: RESP:$RESP SENT:$SENT"
+		unset SENT
 	else
-		echo ""
 		# What to do, when file is greater than max attachement size.
-		[ $R_SCP -eq 1 ] && [ -f "$1" ] && run_scp "$1"
-		[ $R_FTP -eq 1 ] && [ -f "$1" ] && run_ftp "$1"
+		[ $DEBUG -eq 1 ] && echo "DEBUG: File $1 bigger than max attachement size. SIZE:$FILESIZE"
+		SENT=0
+		if [ $R_SCP -eq 1 -a -f "$1" -a $SENT=0 ]
+		then
+			[ $DEBUG -eq 1 ] && echo "DEBUG: SCP module"
+			run_scp "$1" 0
+			[ $? -eq 0 ] && SENT=1 && echo "Success with SCP."
+		fi
+		if [ $R_FTP -eq 1 -a -f "$1" -a $SENT=0 ]
+		then
+			[ $DEBUG -eq 1 ] && echo "DEBUG: FTP module"
+			run_ftp "$1" 0
+			[ $? -eq 0 ] && SENT=1 && echo "Success with FTP."
+		fi
 		#[ $R_DB_ext -eq 1 ] && [ -f "$1" ] && [ $DB_EXT_ACTIVE -eq 1 ] && run_dropbox_ext "$1" 0
-		[ $? -eq 1 ] && [ $R_DB -eq 1 ] && [ -f "$1" ] && run_dropbox_file "$1"
+		[ $SENT -eq 0 ] && [ $R_DB -eq 1 ] && [ -f "$1" ] && run_dropbox_file "$1"
 	fi
 	unset FILESIZE
 	unset FILE_NAME_sf
@@ -138,7 +200,7 @@ function remote_send_file {
 
 function run_send_later {
 	./send_later.sh $MAX_ATACHEMENT_SIZE
-	[ $? -ge 1 ] && CONNECTION_SMTP=0
+	[ $? -ge 1 ] && CONNECTION_SMTP=0 && echo "SendLater returned 1+  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 }
 
 function run_dropbox {
@@ -151,15 +213,22 @@ function run_dropbox_file {
 
 function run_scp {
 	# 1-file
+	# 2-  1 if to delete if succeded
 	echo "Send file $1 by SCP, not yeat implemented."
+	return 1
 }
 
 function run_ftp {
 	# 1-file
+	# 2-  1 if to delete if succeded
 	echo "Send file $1 by FTP, not yeat implemented."
+	return 1
 }
 
 
+
+DEBUG=1
+[ $DEBUG -eq 1 ] && echo "--- DEBUG MODE ON ---"
 ################
 # Variables
 ########
@@ -262,15 +331,17 @@ sleep $T_INITIAL_DELAY
 # Check for connection, remote file config if necessary, smtp
 ########
 check_connection
-if [ $CONNECTION_MODE -eq "1" ]
+check_smtp
+if [ $CONNECTION_MODE -eq 1 ]
 then
-	[ $REMOTE_CONFIG -eq 3 ] && remote_config
-	[ $REMOTE_CONFIG -eq 2 ] && remote_config && ACTIVATE=1
-	[ $REMOTE_CONFIG -eq 1 ] && remote_config
+	[ $REMOTE_CONFIG -eq 3 ] && remote_config 1
+	[ $REMOTE_CONFIG -eq 2 ] && remote_config 1 && ACTIVATE=1
+	[ $REMOTE_CONFIG -eq 1 ] && remote_config 1
 	[ $REMOTE_CONFIG -eq 0 ] && ACTIVATE=1
 	[ $ACTIVATE -eq 0 ] && exit 0;
+	REMOTE_COUNTER=$REMOTE_CHECK_EVERY
 fi
-check_smtp
+
 
 
 ################
@@ -285,34 +356,34 @@ check_smtp
 ########
 while true
 do
+	[ $DEBUG -eq 1 ] && echo "DEBUG: New big loop."
+	[ $DEBUG -eq 1 ] && echo "DEBUG: CONNECTION: $CONNECTION_MODE, SMTP:$CONNECTION_SMTP."
+	[ $DEBUG -eq 1 ] && echo "DEBUG: SMTP_COUNTER:$SMTP_COUNTER"
 	start_time=$(date +%s)
 
 	################
 	# Rules: connecition
 	########
-	if [[ $CONNECTION_MODE -eq "0" ]] && [[ $CONNECTION_SMTP -eq "1" ]]
+	if [[ $CONNECTION_MODE -eq 0 ]] && [[ $CONNECTION_SMTP -eq 1 ]]
 	then
+		[ $DEBUG -eq 1 ] && echo "DEBUG: SMTP and CON_MOD doesnt match... do check them all."
 		check_connection
 		check_smtp
 		SMTP_COUNTER=0
-	fi
-	if [[ $CONNECTION_MODE -eq "0" ]] && [[ $CONNECTION_SMTP -eq "1" ]]
-	then
-		# If sendemail succeded twice, but ping not → ping signals must be blocked
-		CONNECTION_MODE=1
-		CONNECTION_SMTP=1
 	fi
 
 	SMTP_COUNTER=$(($SMTP_COUNTER+1))
 
 	if [ $((SMTP_COUNTER)) -ge $((T_SMTP_CHECK_EVERY)) ]
 	then
+		[ $DEBUG -eq 1 ] && echo "DEBUG: SMTP routine check. "
 		SMTP_COUNTER=0
 		check_smtp
 	fi
-	if [ $CONNECTION_SMTP -eq "0" ]
+	if [[ $CONNECTION_SMTP -eq 0 ]]
 	then
 		# If we didn't established smtp connection, we will check at next loop.
+		[ $DEBUG -eq 1 ] && echo "DEBUG: Because of no SMTP_CONNECTION, we are going to check it again next time."
 		SMTP_COUNTER=$T_SMTP_CHECK_EVERY
 	fi
 
@@ -327,17 +398,17 @@ do
 	# Remote module
 	########
 	REMOTE_COUNTER=$(($REMOTE_COUNTER+1))
-	[ $CONNECTION_MODE -eq "1" ] && [ $REMOTE_COUNTER -ge $REMOTE_CHECK_EVERY ] && REMOTE_COUNTER=0 && remote_config;
+	[ $CONNECTION_MODE -eq 1 ] && [ $REMOTE_COUNTER -ge $REMOTE_CHECK_EVERY ] && REMOTE_COUNTER=0 && remote_config 0;
 	[ $ACTIVATE -eq 0 ] && [ $REMOTE_CONFIG -eq 3 ] && sleep $REMOTE_WAIT_TO_NEXT_CHECK && continue;
-	[ $ACTIVATE -eq 0 ] && [ $CONNECTION_MODE -eq "1" ] && [ $REMOTE_CONFIG -eq 1 ] && exit 0;
-	[ $CONNECTION_MODE -eq "0" ] && [ $REMOTE_COUNTER -ge $REMOTE_CHECK_EVERY ] && DB_EXT_ACTIVE=0
+	[ $ACTIVATE -eq 0 ] && [ $CONNECTION_MODE -eq 1 ] && [ $REMOTE_CONFIG -eq 1 ] && exit 0;
+	[ $CONNECTION_MODE -eq 0 ] && [ $REMOTE_COUNTER -ge $REMOTE_CHECK_EVERY ] && DB_EXT_ACTIVE=0
 	[ $REMOTE_COUNTER -ge $REMOTE_CHECK_EVERY ] && REMOTE_COUNTER=0
 
 
 	################
 	# Rules: sending information
 	########
-	if [[ $CONNECTION_MODE -eq "1" ]] && [[ $CONNECTION_SMTP -eq "1" ]]
+	if [[ $CONNECTION_MODE -eq 1 ]] && [[ $CONNECTION_SMTP -eq 1 ]]
 	then
 		if [ $((CAM_FIRST)) -ge $((1)) ]
 		then
@@ -353,7 +424,6 @@ do
 			./camera.sh 0 1
 		fi
 		run_send_later
-		[[ $? -ge 1 ]] && CONNECTION_SMTP=0
 	else
 		# When no smtp or connection was established - uncomment if you want to make pictures offline
 		#if [ $((CAM_COUNTER)) -ge $((T_CAM_EVERY)) ]
@@ -364,7 +434,7 @@ do
 		sleep 0
 	fi
 
-	if [[ $CONNECTION_MODE -eq "1" ]] && [[ $CONNECTION_SMTP -eq "0" ]]
+	if [[ $CONNECTION_MODE -eq 1 ]] && [[ $CONNECTION_SMTP -eq 0 ]]
 	then
 	# when smtp was not succesfull and there is connection
 		[[ $R_DB -eq 1 ]] && run_dropbox
@@ -373,7 +443,7 @@ do
 		sleep 0
 	fi
 
-	if [ $CONNECTION_MODE -eq "0" ]
+	if [ $CONNECTION_MODE -eq 0 ]
 	then
 	# when connection is down, still try to upload by dropbox, mayby he will be able to connect
 		[[ $R_DB -eq 1 ]] && run_dropbox
